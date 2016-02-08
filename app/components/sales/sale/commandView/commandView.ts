5bd6f1e6d48f10290f8a35ad5aa351d3ca5d32f8
Charly Ghislain <charlyghislain@gmail.com>
2016-02-08 12:34:37 +0100

    fix

--- app/components/sales/sale/commandView/commandView.ts
+++ app/components/sales/sale/commandView/commandView.ts
@@ -396,7 +396,7 @@ export class CommandView {
     onItemRemoved() {
         var searchResult = this.activeSaleService.saleItemsResult;
         if (searchResult.list.size === 0) {
-            this.saleEmptied.next(null);
+            this.saleEmptied.emit(null);
         }
     }
 }
