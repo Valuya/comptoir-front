0c8440df11af40956fbbe507b75bdd0bafbfffc6
Charly Ghislain <charlyghislain@gmail.com>
2016-03-22 11:21:56 +0100

    Fix form messages + broken form controls

--- app/components/utils/formMessage/formMessage.ts
+++ app/components/utils/formMessage/formMessage.ts
@@ -1,19 +1,21 @@
 /**
  * Created by cghislai on 28/08/15.
  */
-import {Component, Host} from 'angular2/core';
-import {NgForm,  AbstractControl} from 'angular2/common';
+import {Component, Host} from "angular2/core";
+import {NgForm, AbstractControl} from "angular2/common";
+import {AuthService} from "../../../services/auth";
 
 @Component({
     selector: 'form-message',
-    inputs: ['controlPath: for', 'checkErrors', 'message', 'error', 'info', 'locale'],
+    inputs: ['controlPath: for', 'checkErrors', 'message', 'error', 'info'],
     templateUrl: './components/utils/formMessage/formMessage.html',
     styleUrls: ['./components/utils/formMessage/formMessage.css']
 })
 export class FormMessage {
     static ERROR_MESSAGES = {
         'required': {
-            'fr': 'Veuillez entrer une valeur'
+            'fr': 'Veuillez entrer une valeur',
+            'en': 'Please enter a value'
         },
         'passwordTooShort': {
             'fr': 'Le mot de passe est trop court'
@@ -21,27 +23,42 @@ export class FormMessage {
     };
 
     controlPath:string;
-    checkErrors:string[];
+    checkErrors:string;
     message:string;
     inlinePos:string = 'false';
     error:boolean = true;
     info:boolean = false;
     formDir:NgForm;
-    locale:string;
+    formControl:AbstractControl;
+    private authService:AuthService;
 
 
-    constructor(@Host() formDir:NgForm) {
+    constructor(@Host() formDir:NgForm,
+                authService:AuthService) {
         this.formDir = formDir;
+        this.authService = authService;
+    }
+
+    checkControl() {
+        if (this.formControl == null) {
+            this.formControl = this.formDir.form.find(this.controlPath);
+        }
+    }
+
+    get control() {
+        this.checkControl();
+        return this.formControl;
     }
 
     get errorMessage() {
         if (this.checkErrors == null) {
             return this.message;
         }
-        var c:AbstractControl = this.formDir.form.find(this.controlPath);
-        for (var i = 0; i < this.checkErrors.length; ++i) {
-            if (this.isPresent(c) && c.touched && c.hasError(this.checkErrors[i])) {
-                return this.getMessage(this.checkErrors[i]);
+        var errorsString = this.checkErrors.replace(/'/g, "\"");
+        var errorArray = JSON.parse(errorsString);
+        for (var i = 0; i < errorArray.length; ++i) {
+            if (this.control.hasError(errorArray[i])) {
+                return this.getMessage(errorArray[i]);
             }
         }
         return null;
@@ -63,7 +80,8 @@ export class FormMessage {
         if (messages == null) {
             return null;
         }
-        return messages[this.locale];
+        var language = this.authService.getEmployeeLanguage();
+        return messages[language.locale];
     }
 }
 
