5b864cfa2b7d2b03b67b935efd8860532db43cf5
cghislai <charlyghislain@gmail.com>
2019-08-09 15:02:10 +0200

    Handle key down on sale transaction input

--- src/app/comptoir/comptoir-menu.ts
+++ src/app/comptoir/comptoir-menu.ts
@@ -41,5 +41,5 @@ export const ComptoirNewSaleRouteItem: MenuItem = {
 
 export const ComptoirMenuItems: MenuItem[] = [
   ComptoirInfoRouteItem,
-  ComptoirSaleRouteItem,
+  ComptoirNewSaleRouteItem,
 ];
